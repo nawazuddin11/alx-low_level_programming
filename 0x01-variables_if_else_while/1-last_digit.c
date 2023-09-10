b8e33af8a6c4568a928d95ebfbfe64c2965670d8
Abdullahi Muhammad <abdulzaifada81@gmail.com>
2023-09-10 09:43:27 -0700

    printing last digit of generated ran no

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Alwayas 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i = n % 10;
+	if (i > 5)
+		printf("last digit of %d is %d and is greater than 5", n, i);
+	else if (i == 0)
+		printf("last digit of %d is %d and is 0", n, i);
+	else
+		printf("last digit of %d is %d and is less than 6 and not 0", n, i);
+	return (0);
+}
