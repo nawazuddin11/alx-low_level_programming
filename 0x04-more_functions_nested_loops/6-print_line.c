5199e358cf87464e08ba3ef63fa4893d12617d47
Abdullahi Muhammad <abdulzaifada81@gmail.com>
2023-09-14 08:46:37 -0700

    y

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * @n: number of line to be printed
+ * Return: int
+ */
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		if (n < 0 || n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			 -putchar('_');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
