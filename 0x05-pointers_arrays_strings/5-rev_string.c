60aff04d28eb42ae052b35148a46ceeac7ba634f
nawazuddin11 <abdulzaifada81@gmail.com>
2025-04-10 03:04:07 -0700

    poiters

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,28 @@
+
 #include "main.h"
 /**
- * rev_string - reverse string
- * @s: is the string
- * Return: void
+ * rev_string - reverses a string
+ * _putchar - print each character
+ * @s: char to check
+ *
+ * Description: This will reverse a string
+ * Return: 0 is success
  */
 void rev_string(char *s)
 {
-	char rev =  s[0];
-	int i = 0;
-	int j;
+	int a = 0, b, c;
+	char d;
 
-	while (s[i] != '\0')
+	while (s[a] != '\0')
 	{
-		i++;
-		for (j = 0; j < i; j++)
-		{
-			i--;
-			rev = s[j];
-			s[j] = s[i];
-			s[i] = rev;
-		}
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
 	}
 }
+
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,16 @@
 #include "main.h"
 /**
- * *_strcpy - copies a string 
- *@dest: copy to
- *@src: copy from
- * Return: char
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	int a;
 
-	while (*(src + 1) != '\0')
-	{
-		l++;
-	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (dest);
 }
