491d5f5e47d887f4c8f113f52da219e1908415d6
abdullahi <abdulzaifada81@gmail.com>
2025-07-23 16:57:18 -0700

    more on mallac

--- 0x13-more_singly_linked_lists/0-print_list.c
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -2,24 +2,22 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_listint - Prints all the elements of a listint_t list
+ * @h: Pointer to the head of the list
  *
- * Return: the number of nodes printed
+ * Return: Number of nodes in the list
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t s = 0;
+    size_t count = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
-	}
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
 
-	return (s);
+    return (count);
 }
+
--- 0x13-more_singly_linked_lists/1-list_len.c
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * listint_len - Returns the number of elements in a listint_t list
+ * @h: Pointer to the head of the list
  *
- * Return: number of elements in h
+ * Return: Number of elements in the list
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t n = 0;
+    size_t count = 0;
 
-	while (h)
-	{
-		n++;
-		h = h->next;
-	}
-	return (n);
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
 }
+
